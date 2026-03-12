45c1f42b6e0b31e863beeb2c708c4e0d6a4a9101
Kamal <247006111122@student.unsil.ac.id>
2026-03-12 13:56:04 +0700

    Membuat file vercel, agar bisa deploy

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 
- Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::delete('/delete-account', [AuthController::class,'deleteAccount'])->middleware('auth:api');
 Route::get('/me',[AuthController::class,'me'])->middleware('auth:api');
 Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:api');
 
-    Route::post('/logout',[AuthController::class,'logout']);
-    Route::post('/refresh',[AuthController::class,'refresh']);
+Route::post('/logout',[AuthController::class,'logout']);
+Route::post('/refresh',[AuthController::class,'refresh']);
     
-    Route::get('/tasks',[TaskController::class,'index']);
-    Route::post('/tasks',[TaskController::class,'store']);
-    Route::put('/tasks/{id}',[TaskController::class,'update']);
-    Route::delete('/tasks/{id}',[TaskController::class,'destroy']);
+Route::get('/tasks',[TaskController::class,'index']);
+Route::post('/tasks',[TaskController::class,'store']);
+Route::put('/tasks/{id}',[TaskController::class,'update']);
+Route::delete('/tasks/{id}',[TaskController::class,'destroy']);
